a8aadccc04e5abaf5b09022925fbea13d7766044
divyansh singh <52194749+devsingh1234@users.noreply.github.com>
2022-03-18 02:04:50 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Canvas } from '@react-three/fiber';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Canvas>
+          <ambientLight intensity={0.1} />
+          <directionalLight color="red" position={[10, 0, 5]} />
+          <mesh>
+            <boxGeometry />
+            <meshStandardMaterial />
+          </mesh>
+        </Canvas>
       </header>
     </div>
   );
